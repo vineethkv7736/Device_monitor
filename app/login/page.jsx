171acce6ce5070845a739dbadd5fbdcabc5d2dec
niranjan-official <niranjansabarianth1521@gmail.com>
2024-01-16 15:42:21 +0530

    login page design change

---
+++ app/login/page.jsx
@@ -0,0 +1,141 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { auth, provider } from "@/firebase/config";
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
+import React, { useState } from "react";
+import SiginIn from "@/components/siginIn";
+import Image from "next/image";
+import Link from "next/link";
+
+const Home = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    email: "",
+    pass: "",
+  });
+
+  const [log, setLog] = useState(1);
+  const [error, seterror] = useState("");
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      router.push("/dashboard");
+    }
+  });
+
+  const sigingoogle = (e) => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        router.push("/dashboard");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    setLog(0);
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, formData.email, formData.pass)
+      .then((userCredential) => {
+        router.push("/dashboard");
+      })
+      .catch((error) => {
+        setLog(1);
+        seterror("Incorrect Username/Password");
+      });
+  };
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      {log ? (
+     
+        <div className="w-full h-max flex flex-col sm:h-max sm:w-96 sm:shadow-xl sm:pb-4">
+          <div className="w-full h-1/2 sm:hidden flex justify-center items-center">
+            <Image
+              src={"/login.svg"}
+              height={0}
+              width={0}
+              style={{ height: "100%", width: "auto" }}
+              alt="login"
+            />
+          </div>
+          <div className="w-full h-1/2 flex flex-col text-center px-10">
+            <h1 className="text-4xl text-blue-500 font-semibold mt-3">Login</h1>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col justify-center items-center mt-4"
+            >
+              <input
+                type="text"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className=""
+                placeholder="Email"
+              />
+
+              <input
+                type="Password"
+                name="pass"
+                value={formData.pass}
+                onChange={handleChange}
+                className=""
+                placeholder="Password"
+              />
+
+              <p className="text-red-600 text-sm">{error}</p>
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white p-2 rounded-3xl"
+              >
+                Log In
+              </button>
+              <div className="w-full flex items-center justify-center gap-3 my-3">
+                <hr style={{height:'1px'}} className="w-36"/>
+                <span>or</span>
+                <hr style={{height:'1px'}} className="w-36"/>
+              </div>
+             
+                <button
+                  onClick={sigingoogle}
+                  className=" flex gap-2 bg-blue-500 text-white p-1 pr-2 items-center mb-3"
+                >
+                  <div className="bg-white p-1">
+                  <Image src={'/google.svg'} width={35} height={35} alt="G"/>
+                  </div>
+                  <span className="px-3">
+                  Continue with Google
+                  </span>
+                </button>
+                <span className="flex gap-1">
+                  <span className="text-gray-400">Don't have an Account?</span>
+                <Link href={'#'}>
+                <span className="text-blue-500 font-semibold">Signup</span>
+                </Link>
+                </span>
+            
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <SiginIn />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
